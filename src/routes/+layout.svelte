43317904f3a19ba2ca96c1af2bf59bec09a20b18
Demi Ogun <demiogun1@gmail.com>
2024-05-17 16:10:50 +0100

    contact page + svelte speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
   import '../app.css';
   import Header from '$lib/components/Header.svelte';
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+  injectSpeedInsights();
 </script>
 
 <Header />
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,16 @@
+<div>
+  <div class="mb-4">
+    <h2 class="text-xl">About</h2>
+    <p>I'm a photographer based in Clerkenwell, London.</p>
+    <p>
+      All of the pictures on this site were shot on 35mm film using both manual SLR and automatic
+      point-and-shoot cameras.
+    </p>
+  </div>
+  <div>
+    <h2 class="text-xl">Contact</h2>
+    <a class="text-orange-400" href="mailto:demiogun.photography@gmail.com"
+      >demiogun.photography@gmail.com</a
+    >
+  </div>
+</div>
